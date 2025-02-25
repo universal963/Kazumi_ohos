f388c0d9fdfc6a12293796e63efdee0248cb78a7
Predidit <34627277+Predidit@users.noreply.github.com>
2025-02-25 10:36:52 +0800

    better page transitions
    (cherry picked from commit 4b94c77b6c738e2c1fddf665267c35a3b625436e)

--- lib/pages/index_module.dart
+++ lib/pages/index_module.dart
@@ -39,7 +39,7 @@ class IndexModule extends Module {
           ChildRoute(
             "/error",
             child: (_) => Scaffold(
-              appBar: AppBar (title: const Text("Kazumi")),
+              appBar: AppBar(title: const Text("Kazumi")),
               body: const Center(child: Text("初始化失败")),
             ),
           ),
@@ -47,7 +47,7 @@ class IndexModule extends Module {
         transition: TransitionType.noTransition);
     r.child("/tab", child: (_) {
       return const IndexPage();
-    }, children: menu.routes, transition: TransitionType.noTransition);
+    }, children: menu.routes);
     r.module("/video", module: VideoModule());
     r.module("/info", module: InfoModule());
     r.module("/settings", module: SettingsModule());
