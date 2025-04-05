381e011d7c957c411ca13504e2a5e0872bfa8d66
Predidit <34627277+predidit@users.noreply.github.com>
2025-04-05 17:55:40 +0800

    add lazy loader for info page
    (cherry picked from commit 73cfa5dd22e074d6d73b95fe966eb63a7fd6accb)

--- lib/pages/info/info_tabview.dart
+++ lib/pages/info/info_tabview.dart
@@ -1,81 +1,51 @@
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kazumi/bean/widget/error_widget.dart';
-import 'package:kazumi/pages/info/info_controller.dart';
 import 'package:kazumi/bean/card/comments_card.dart';
 import 'package:kazumi/bean/card/character_card.dart';
 import 'package:kazumi/utils/utils.dart';
 import 'package:skeletonizer/skeletonizer.dart';
+import 'package:kazumi/modules/bangumi/bangumi_item.dart';
+import 'package:kazumi/modules/comments/comment_item.dart';
+import 'package:kazumi/modules/characters/character_item.dart';
 
 class InfoTabView extends StatefulWidget {
-  const InfoTabView({super.key});
+  const InfoTabView({
+    super.key,
+    required this.commentsIsLoading,
+    required this.charactersIsLoading,
+    required this.commentsQueryTimeout,
+    required this.charactersQueryTimeout,
+    required this.tabController,
+    required this.loadMoreComments,
+    required this.loadCharacters,
+    required this.bangumiItem,
+    required this.commentsList,
+    required this.characterList,
+    required this.isLoading,
+  });
+
+  final bool commentsIsLoading;
+  final bool charactersIsLoading;
+  final bool commentsQueryTimeout;
+  final bool charactersQueryTimeout;
+  final TabController tabController;
+  final Future<void> Function({int offset}) loadMoreComments;
+  final Future<void> Function() loadCharacters;
+  final BangumiItem bangumiItem;
+  final List<CommentItem> commentsList;
+  final List<CharacterItem> characterList;
+  final bool isLoading;
 
   @override
   State<InfoTabView> createState() => _InfoTabViewState();
 }
 
-class _InfoTabViewState extends State<InfoTabView> {
-  final infoController = Modular.get<InfoController>();
+class _InfoTabViewState extends State<InfoTabView>
+    with SingleTickerProviderStateMixin {
   final maxWidth = 950.0;
-  bool commentsIsLoading = false;
-  bool charactersIsLoading = false;
-  bool commentsQueryTimeout = false;
-  bool charactersQueryTimeout = false;
   bool fullIntro = false;
   bool fullTag = false;
-
-  @override
-  void initState() {
-    super.initState();
-    if (infoController.commentsList.isEmpty) {
-      loadMoreComments();
-    }
-    if (infoController.characterList.isEmpty) {
-      loadCharacters();
-    }
-  }
-
-  Future<void> loadCharacters() async {
-    infoController
-        .queryBangumiCharactersByID(infoController.bangumiItem.id)
-        .then((_) {
-      if (infoController.characterList.isEmpty && mounted) {
-        setState(() {
-          charactersIsLoading = false;
-          charactersQueryTimeout = true;
-        });
-      }
-      if (infoController.characterList.isNotEmpty && mounted) {
-        setState(() {
-          charactersIsLoading = false;
-        });
-      }
-    });
-  }
-
-  Future<void> loadMoreComments({int offset = 0}) async {
-    infoController
-        .queryBangumiCommentsByID(infoController.bangumiItem.id, offset: offset)
-        .then((_) {
-      if (infoController.commentsList.isEmpty && mounted) {
-        setState(() {
-          commentsIsLoading = false;
-          commentsQueryTimeout = true;
-        });
-      }
-      if (infoController.commentsList.isNotEmpty && mounted) {
-        setState(() {
-          commentsIsLoading = false;
-        });
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
 
   Widget get infoBody {
     return Center(
@@ -93,7 +63,7 @@ class _InfoTabViewState extends State<InfoTabView> {
               // https://stackoverflow.com/questions/54091055/flutter-how-to-get-the-number-of-text-lines
               // only show expand button when line > 7
               LayoutBuilder(builder: (context, constraints) {
-                final span = TextSpan(text: infoController.bangumiItem.summary);
+                final span = TextSpan(text: widget.bangumiItem.summary);
                 final tp =
                     TextPainter(text: span, textDirection: TextDirection.ltr);
                 tp.layout(maxWidth: constraints.maxWidth);
@@ -113,7 +83,7 @@ class _InfoTabViewState extends State<InfoTabView> {
                             scrollbars: false,
                           ),
                           child: SelectableText(
-                            infoController.bangumiItem.summary,
+                            widget.bangumiItem.summary,
                             textAlign: TextAlign.start,
                             scrollPhysics: NeverScrollableScrollPhysics(),
                             selectionHeightStyle: ui.BoxHeightStyle.max,
@@ -132,7 +102,7 @@ class _InfoTabViewState extends State<InfoTabView> {
                   );
                 } else {
                   return SelectableText(
-                    infoController.bangumiItem.summary,
+                    widget.bangumiItem.summary,
                     textAlign: TextAlign.start,
                     scrollPhysics: NeverScrollableScrollPhysics(),
                     selectionHeightStyle: ui.BoxHeightStyle.max,
@@ -146,8 +116,8 @@ class _InfoTabViewState extends State<InfoTabView> {
                 spacing: 8.0,
                 runSpacing: Utils.isDesktop() ? 8 : 0,
                 children: List<Widget>.generate(
-                    fullTag || infoController.bangumiItem.tags.length < 13
-                        ? infoController.bangumiItem.tags.length
+                    fullTag || widget.bangumiItem.tags.length < 13
+                        ? widget.bangumiItem.tags.length
                         : 13, (int index) {
                   if (!fullTag && index == 12) {
                     // make tag expandable
@@ -168,9 +138,9 @@ class _InfoTabViewState extends State<InfoTabView> {
                     label: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        Text('${infoController.bangumiItem.tags[index].name} '),
+                        Text('${widget.bangumiItem.tags[index].name} '),
                         Text(
-                          '${infoController.bangumiItem.tags[index].count}',
+                          '${widget.bangumiItem.tags[index].count}',
                           style: TextStyle(
                               color: Theme.of(context).colorScheme.primary),
                         ),
@@ -207,7 +177,7 @@ class _InfoTabViewState extends State<InfoTabView> {
               const SizedBox(height: 16),
               Skeletonizer.zone(child: Bone.text(fontSize: 18, width: 50)),
               const SizedBox(height: 8),
-              if (infoController.isLoading)
+              if (widget.isLoading)
                 Skeletonizer.zone(
                   child: Wrap(
                     spacing: 8.0,
@@ -230,12 +200,8 @@ class _InfoTabViewState extends State<InfoTabView> {
           onNotification: (scrollEnd) {
             final metrics = scrollEnd.metrics;
             if (metrics.pixels >= metrics.maxScrollExtent - 200) {
-              if (!commentsIsLoading) {
-                setState(() {
-                  commentsIsLoading = true;
-                });
-                loadMoreComments(offset: infoController.commentsList.length);
-              }
+              widget.loadMoreComments(
+                  offset: widget.commentsList.length);
             }
             return true;
           },
@@ -250,7 +216,8 @@ class _InfoTabViewState extends State<InfoTabView> {
                     NestedScrollView.sliverOverlapAbsorberHandleFor(context),
               ),
               SliverLayoutBuilder(builder: (context, _) {
-                if (infoController.commentsList.isEmpty && commentsIsLoading) {
+                if (widget.commentsList.isEmpty &&
+                    widget.commentsIsLoading) {
                   return SliverList.builder(
                     itemCount: 4,
                     itemBuilder: (context, _) {
@@ -272,19 +239,15 @@ class _InfoTabViewState extends State<InfoTabView> {
                     },
                   );
                 }
-                if (commentsQueryTimeout) {
+                if (widget.commentsQueryTimeout) {
                   return SliverFillRemaining(
                     child: GeneralErrorWidget(
                       errMsg: '获取失败，请重试',
                       actions: [
                         GeneralErrorButton(
                           onPressed: () {
-                            setState(() {
-                              commentsIsLoading = true;
-                              commentsQueryTimeout = false;
-                            });
-                            loadMoreComments(
-                                offset: infoController.commentsList.length);
+                            widget.loadMoreComments(
+                                offset: widget.commentsList.length);
                           },
                           text: '重试',
                         ),
@@ -294,7 +257,7 @@ class _InfoTabViewState extends State<InfoTabView> {
                 }
                 return SliverList.separated(
                   addAutomaticKeepAlives: false,
-                  itemCount: infoController.commentsList.length,
+                  itemCount: widget.commentsList.length,
                   itemBuilder: (context, index) {
                     return SafeArea(
                       top: false,
@@ -307,7 +270,7 @@ class _InfoTabViewState extends State<InfoTabView> {
                                 ? maxWidth
                                 : MediaQuery.sizeOf(context).width - 32,
                             child: CommentsCard(
-                              commentItem: infoController.commentsList[index],
+                              commentItem: widget.commentsList[index],
                             ),
                           ),
                         ),
@@ -354,7 +317,8 @@ class _InfoTabViewState extends State<InfoTabView> {
               handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
             ),
             SliverLayoutBuilder(builder: (context, _) {
-              if (infoController.characterList.isEmpty && charactersIsLoading) {
+              if (widget.characterList.isEmpty &&
+                  widget.charactersIsLoading) {
                 return SliverList.builder(
                   itemCount: 4,
                   itemBuilder: (context, _) {
@@ -376,18 +340,14 @@ class _InfoTabViewState extends State<InfoTabView> {
                   },
                 );
               }
-              if (charactersQueryTimeout) {
+              if (widget.charactersQueryTimeout) {
                 return SliverFillRemaining(
                   child: GeneralErrorWidget(
                     errMsg: '获取失败，请重试',
                     actions: [
                       GeneralErrorButton(
                         onPressed: () {
-                          setState(() {
-                            charactersIsLoading = true;
-                            charactersQueryTimeout = false;
-                          });
-                          loadCharacters();
+                          widget.loadCharacters();
                         },
                         text: '重试',
                       ),
@@ -396,7 +356,7 @@ class _InfoTabViewState extends State<InfoTabView> {
                 );
               }
               return SliverList.builder(
-                itemCount: infoController.characterList.length,
+                itemCount: widget.characterList.length,
                 itemBuilder: (context, index) {
                   return Center(
                     child: Padding(
@@ -406,7 +366,7 @@ class _InfoTabViewState extends State<InfoTabView> {
                             ? maxWidth
                             : MediaQuery.sizeOf(context).width - 32,
                         child: CharacterCard(
-                          characterItem: infoController.characterList[index],
+                          characterItem: widget.characterList[index],
                         ),
                       ),
                     ),
@@ -423,6 +383,7 @@ class _InfoTabViewState extends State<InfoTabView> {
   @override
   Widget build(BuildContext context) {
     return TabBarView(
+      controller: widget.tabController,
       children: [
         Builder(
           // This Builder is needed to provide a BuildContext that is
@@ -447,7 +408,7 @@ class _InfoTabViewState extends State<InfoTabView> {
                   child: SafeArea(
                     top: false,
                     bottom: false,
-                    child: infoController.isLoading ? infoBodyBone : infoBody,
+                    child: widget.isLoading ? infoBodyBone : infoBody,
                   ),
                 ),
               ],
