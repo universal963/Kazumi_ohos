ba814a0d91346d6499f81d0d8f6c4c462ea058a0
leetao <leetao94cn@gmail.com>
2025-08-10 11:22:09 +0800

    feat: add SHA256 hash verification for update downloads (#1167)
    (cherry picked from commit 0ba21183f6a338200327f9cd827ab39ab027e19e)

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -221,7 +221,7 @@ class Utils {
   // 版本对比
   static bool needUpdate(localVersion, remoteVersion) {
     List<String> localVersionList = localVersion.split('.');
-    List<String> remoteVersionList = remoteVersion.split('v')[1].split('.');
+    List<String> remoteVersionList = remoteVersion.split('.');
     for (int i = 0; i < localVersionList.length; i++) {
       int localVersion = int.parse(localVersionList[i]);
       int remoteVersion = int.parse(remoteVersionList[i]);
@@ -544,5 +544,12 @@ class Utils {
     } catch (e) {
       return dateString;
     }
+  }
+
+  /// 计算文件的 SHA256 哈希值
+  static Future<String> calculateFileHash(File file) async {
+    final bytes = await file.readAsBytes();
+    final digest = sha256.convert(bytes);
+    return digest.toString();
   }
 }
